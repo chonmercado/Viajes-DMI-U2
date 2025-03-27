6585025c68aa224202cf75b60d2d840554262683
Alondra Domínguez <145074402+Alondra180323@users.noreply.github.com>
2025-03-26 19:57:15 -0600

    Update ErrorMessages.js Se actualizaron complementos de la aplicación

--- models/ErrorMessages.js
+++ models/ErrorMessages.js
@@ -1,10 +1,10 @@
 export const ErrorMessages = {
-    "auth/invalid-email": "El correo electrónico no es válido.",
+    "auth/invalid-email": "El correo electrónico no es válido.Verificalo de nuevo",
     "auth/email-already-in-use": "El correo electrónico ya está en uso.",
     "auth/weak-password": "La contraseña es demasiado débil. Use al menos 6 caracteres.",
-    "auth/missing-email": "El correo electrónico es obligatorio.",
+    "auth/missing-email": "El correo electrónico es obligatorio.El campo no se debe dejar vacio",
     "auth/wrong-password": "La contraseña es incorrecta.",
     "auth/user-not-found": "No se ha encontrado un usuario con ese correo electrónico.",
-    "auth/invalid-credential": "Las credenciales de inicio de sesión son incorrectas.",
+    "auth/invalid-credential": "Las credenciales de inicio de sesión son incorrectas. Verificalas de nuevo",
     "INVALID_LOGIN_CREDENTIALS": "Las credenciales de inicio de sesión son incorrectas. Verifique su correo electrónico y contraseña.",
   };
